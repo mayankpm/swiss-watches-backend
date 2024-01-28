d2744d872ab45cd4004e9c4a81f170df82884dd0
your_github_username <your_github_email>
2024-01-28 15:29:05 +0530

    postgres option

--- Backend/Hello/Hello/settings.py
+++ Backend/Hello/Hello/settings.py
@@ -113,17 +113,19 @@ WSGI_APPLICATION = 'Hello.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'abc',
+        'USER': 'root',
+        'PASSWORD': 'helloworld',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
-# DATABASES = {
-# 	'default': {
-# 		'ENGINE': 'django.db.backends.mysql',
-# 		'NAME': 'abc',
-# 		'USER': 'root',
-# 		'PASSWORD': 'helloworld',
-# 		'HOST':'localhost',
-# 		'PORT':'3306',
-# 	}
-# }
+
+
 
 
 AUTH_USER_MODEL = 'authentication.User'
